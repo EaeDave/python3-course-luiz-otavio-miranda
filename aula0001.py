e78f502e9d44c23b7e7aacae2de625868009ab10
EaeDave <EaeDav3@gmail.com>
2024-09-04 18:44:46 -0300

    Comentários e DocStrings

---
+++ aula0001.py
@@ -0,0 +1,14 @@
+# Este é um comentário e não será exibido pelo interpretador do Python
+print("Isso será exibido!")
+
+"""
+DocString
+é usado para explicar código, porém pode ser usado como comentário
+de múltiplas linhas
+"""
+
+'''
+É possível
+usar aspas
+simples também.
+'''
