83ba0c2ea6c949ef18f6f6a6a2e191b2504c76bb
EaeDave <EaeDav3@gmail.com>
2024-09-05 19:03:52 -0300

    Tipo str (string) - Introdução aos tipos de dados

---
+++ aula0003.py
@@ -0,0 +1,20 @@
+"""
+Pythhon = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(123)  # Aqui foi impresso um número do tipo int, ou inteiro.
+
+# Aspas simples
+print('123')  # Aqui fo impresso uma cadeia de caracteres de números, ou seja, tipo str ou texto.
+
+# Aspas duplas
+print("Esse é o meu código :)")
+
+# Escape
+print("As horas \"voaram\"")  # Caractere de escape é a barra invertida "\".
+
+# r
+print(r"David \"Rodrigues\"")  # É possível imprimir caracteres especiais dessa forma.
