09b3743819d12f8fcc4b793ddefb37ceb3fb7152
EaeDave <EaeDav3@gmail.com>
2024-09-11 19:06:39 -0300

    Introdução aos operadores aritméticos (matemática)

---
+++ aula0009.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subracao = 10 - 5
+print('Subtração', subracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2  # Sempre retorna float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2  # Retorna float ou int dependendo do valor
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # Resto da divisão
+print('Módulo', modulo)
