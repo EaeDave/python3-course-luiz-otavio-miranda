3866ff80ddccf5cbeb43c629258da2c85715b62c
EaeDave <EaeDav3@gmail.com>
2024-09-11 19:11:25 -0300

    Concatenação (+) e repetição (*) com operadores aritméticos

---
+++ aula0010.py
@@ -0,0 +1,9 @@
+# Concatenar significa, juntar.
+# Strings são concatenáveis.
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10  # Repetição.
+tres_vezes_david = 'David' * 3
+print(a_dez_vezes)
+print(tres_vezes_david)
