04f5f70bafb4dfd9c55eb73800245a47697e7100
EaeDave <eaedav3@gmail.com>
2024-09-15 17:39:12 -0300

    Revertendo commit

--- aula0015.py
+++
@@ -1,9 +0,0 @@
-nome = input('Qual o seu nome? ')
-print(f'Muito prazer, {nome}!')
-
-idade = input('Qual a sua idade? ')
-print(f'Você então tem {idade} anos.')
-
-numero_1 = int(input('Digite um número: '))  # typecasting
-numero_2 = int(input('Digite outro número: '))
-print(f'A soma entre {numero_1} + {numero_2} é igual a = {numero_1 + numero_2}')
