3ae0bbe6dcafacae1c9828010369fd6135ac83ef
EaeDave <EaeDav3@gmail.com>
2024-09-16 19:31:52 -0300

    if, elif e else: entendendo o fluxo do interpretador em condicionais

---
+++ aula0017.py
@@ -0,0 +1,18 @@
+# if / elif       / else
+# se / se não se  / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Este código será exibido caso a condicao1 seja verdadeira')
+elif condicao2:
+    print('Este código será exibido caso a condicao2 seja verdadeira')
+elif condicao3:
+    print('Este código será exibido caso a condicao3 seja verdadeira')
+elif condicao4:
+    print('Este código será exibido caso a condicao4 seja verdadeira')
+else:
+    print('Se nenhuma das condicões anteriores forem verdadeiras, isso será exibido')
