733936ed437455d4bf8ae323ad2e67fe0ff20e0c
EaeDave <EaeDav3@gmail.com>
2024-09-18 20:19:44 -0300

    Operadores in e not in

---
+++ aula0024.py
@@ -0,0 +1,13 @@
+# Operadores in e not in
+# Strings são iteráveis
+#  0 1 2 3 4 
+#  D a v i d
+# -5-4-3-2-1
+
+nome = 'David'
+print(nome[0])  # Foi consultado o index 0 do iteravel "nome" - Primeira letra
+print(nome[-1])  # d - Segunda letra
+
+print('i' in nome)  # A letra "i" está entre os caracteres da variável "nome"? True
+
+print('vid' not in nome)  # False
