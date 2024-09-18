8851eaef72382c6316679da7c30891ce0f4c61a2
EaeDave <EaeDav3@gmail.com>
2024-09-18 20:12:35 -0300

    Operador lógico not

---
+++ aula0023.py
@@ -0,0 +1,12 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+mentiroso = True  # O mentiroso diz que é verdade
+
+print(not mentiroso)  # Se o mentiroso mente, e diz que é verdade, logo é mentira = False
+
+mentiroso2 = False  # O outro mentiroso diz que é mentira
+
+print(not mentiroso2)  # O que ele diz que é mentira, na realidade é verdade = True
