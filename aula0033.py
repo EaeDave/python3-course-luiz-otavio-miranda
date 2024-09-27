2bbfb43cd2e25dc261f7c205462cc0a63ff9a4d0
EaeDave <EaeDav3@gmail.com>
2024-09-27 19:39:31 -0300

    Conversa - tipos built-in, documentação, tipos imutáveis, métodos de string

---
+++ aula0033.py
@@ -0,0 +1,9 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+
+string = 'David Rodrigues'
+# outra_variavel = string
+string[4] = 'a'  # string é um tipo de dado imutável, ou seja, ocorrerá um erro nessa linha
+print(string[4])
