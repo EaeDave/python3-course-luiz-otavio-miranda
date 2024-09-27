6857732ec8f526019d6f9257607f1b617365c832
EaeDave <EaeDav3@gmail.com>
2024-09-27 19:48:50 -0300

    while e break - Estrutura de repetição (Parte 1)

---
+++ aula0034.py
@@ -0,0 +1,17 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+"""
+
+# Criando um for dentro de um while
+valor = 0
+while valor < 11:
+    print(valor)
+    valor += 1
+
+# Looping infinito interrompido
+
+while True:
+    print('Isso seria impressoro inifitamente se não fosse o break.')
+    break
