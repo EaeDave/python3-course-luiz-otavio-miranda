cfe736f21f562860774419beea0dbe9dd0b21bd9
EaeDave <EaeDav3@gmail.com>
2024-10-01 19:35:27 -0300

    Exercício guiado - Calculadora Parte 2

--- aula0040.py
+++ aula0040.py
@@ -1,6 +1,6 @@
 """ Calculadora com while """
-calcular = True
-while calcular:
+
+while True:
     escolha = input('Escolha entre somar (+), subtrair (-), multiplicar (*), dividir (/) ou sair (s): ')
     
     somar = escolha.startswith('+')
@@ -10,11 +10,39 @@ while calcular:
     sair = escolha.startswith('s')
     
     if somar:
-        valor1 = float(input('Digite o 1° valor da soma: '))
-        valor2 = float(input('Digite o 2° valor da soma: '))
-        soma = valor1 + valor2
-        print(f'{valor1} + {valor2} = {soma}')
+        try:
+            valor1 = float(input('Digite o 1° valor da soma: '))
+            valor2 = float(input('Digite o 2° valor da soma: '))
+            soma = valor1 + valor2
+            print(f'{valor1} + {valor2} = {soma}')
+        except:
+            print('Digite somente números!')
+            
+    elif subtrair:
+        try:
+            valor1 = float(input('Digite o 1° valor da subtração: '))
+            valor2 = float(input('Digite o 2° valor da subtração: '))
+            subtracao = valor1 - valor2
+            print(f'{valor1} - {valor2} = {subtracao}')
+        except:
+            print('Digite somente números!')
+    elif multiplicar:
+        try:
+            valor1 = float(input('Digite o 1° valor da multiplicação: '))
+            valor2 = float(input('Digite o 2° valor da multiplicação: '))
+            multiplicacao = valor1 * valor2
+            print(f'{valor1} * {valor2} = {multiplicacao}')
+        except:
+            print('Digite somente números!')
+    elif dividir:
+        try:
+            valor1 = float(input('Digite o 1° valor da divisão: '))
+            valor2 = float(input('Digite o 2° valor da divisão: '))
+            divisao = valor1 / valor2
+            print(f'{valor1} / {valor2} = {divisao}')
+        except:
+            print('Digite somente números!')
     elif sair:
         break
     else:
-        print('Valor inválido.')
+        print('Operador inválido.')
