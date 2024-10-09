a7dbddbeac528153faf5bcfc86b54ac27d397bea
EaeDave <EaeDav3@gmail.com>
2024-10-09 19:16:11 -0300

    Alterando uma lista com índices, del, append e pop (Tipo list)

--- aula048.py
+++ aula048.py
@@ -4,12 +4,27 @@ Tipo list - Mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
 Métodos úteis: append, insert, pop, del, clear, extend, +
+Create Read Update Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
 """
 #         01234
 #        -54321
-string = 'ABCDE'  # 5 caracteres
-lista = [123, True, 'David Rodrigues', 1.2, []]
-# print(bool(lista))  # falsy
-# print(lista, type(lista))
-lista[2] = 'David'  # mudando um elemento da lista
-print(lista)
+
+lista_nomes = ['David', 'Camila', 'Liz', 'Tommy']  
+lista_nomes.append('Bianca')  # Create (C)
+print(lista_nomes[0])  # Read (R)
+lista_nomes[3] = 'Tom'  # Update (U)
+print(lista_nomes)
+lista_nomes.pop()  # Delete (D)
+print(lista_nomes)
+
+# del vs pop()
+
+# em del, é possível remover o item pelo índice, sem retornar o valor:
+del lista_nomes[3]
+print(lista_nomes)
+
+# em pop(), é possível fazer o mesmo, porém retorna o valor deletado, podendo armazenar em variáveis
+
+nome_apagado = lista_nomes.pop(2)
+print(nome_apagado)
