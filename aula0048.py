a7406cf57f234e0df438185b64d29411652373de
EaeDave <EaeDav3@gmail.com>
2024-10-10 20:17:54 -0300

    Cuidados com tipos de dados mutáveis - list e copy

--- aula0048.py
+++ aula0048.py
@@ -1,25 +1,32 @@
 """
-Listas em Python
-Tipo list - Mutável
-Suporta vários valores de qualquer tipo
-Conhecimentos reutilizáveis - índices e fatiamento
-Métodos úteis:
-    append - Adiciona um item ao final
-    insert - Adiciona um item no índice escolhido
-    pop - Remove do final ou do índice escolhido
-    del - apaga um índice
-    clear - limpa a lista
-    extend - estende a lista
-    + - concatena listas
-Create Read Update   Delete
-Criar, ler, alterar, apagar = lista[i] (CRUD)
+Cuidados com dados mutáveis
+= - copiado o valor (imutáveis)
+= - aponta para o mesmo valor na memória (mutável)
 """
-lista_a = [1, 2, 3]
-lista_b = [4, 5, 6]
-lista_c = lista_a + lista_b  # Polimorfismo, nesse caso o operador  + está concatenando as duas listas
-print(lista_c)
 
-lista_nomes = ['David', 'Camila', 'Amaro']
-lista_sobrenomes = ['Rodrigues', 'Santos', 'Silva']
-lista_nomes.extend(lista_sobrenomes)  # Este método não retorna nada, mas muda o valor do objeto
-print(lista_nomes)
+nome = 'David'
+# nome[0] = 'd'  Aqui geraria um erro, porque str é do tipo imutável, do not support item assignment
+nome_antigo = nome  # Aqui está sendo feito uma cópia do valor
+print(nome_antigo)
+nome = 'Laerze'  # Essa variável teve outra atribuição, mas o dado dela não foi mudado
+print(nome)
+
+lista = [1, 2, 3, 4]
+lista_2 = lista  # Aqui está sendo apontado um local de referência na memória
+lista_2[0] = 50
+print(lista)  # A lista foi mudada, porque foi apontado uma referência na memória
+
+print(id(lista))  # Ambos os id serão iguais
+print(id(lista_2))  # Mesmo id
+
+print()
+
+print(id(nome))  # Ids diferentes
+print(id(nome_antigo))  # Ids diferentes
+
+numeros_1 = [1, 2, 3, 4]
+numeros_2 = numeros_1.copy()  # Faz uma cópia simples do valor (retorna o valor)
+numeros_2[0] = 100
+
+print(numeros_1)  # Mudando o valor de numeros_2, não afetou essa variável
+print(numeros_2)
