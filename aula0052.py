3fe79cd3268d8674c1eab788ddbeb8a914f3a943
EaeDave <EaeDav3@gmail.com>
2024-10-12 12:22:52 -0300

    Tipo tuple (tuplas)

---
+++ aula0052.py
@@ -0,0 +1,12 @@
+"""
+Tipo tupla - Uma lista imutável
+"""
+
+tupla = 'Maria', 'Helena', 'Luiz'  # Uma forma de clarar uma tuple
+
+outra_tupla = ('David', 'Camila', 'Liz')  # Outra forma de declarar um tuple
+
+lista = ['Objeto1', 'Objeto2', 'Objeto3']
+
+tupla_convertida = tuple(lista)
+print(tupla_convertida)
