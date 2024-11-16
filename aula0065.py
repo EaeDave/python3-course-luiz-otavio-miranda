fb2e3bb974d0627d784e0e20a5d9550b739e59e3
EaeDave <EaeDav3@gmail.com>
2024-11-16 16:35:34 -0300

    def define uma função personalizada - Introdução às funções em Python

---
+++ aula0065.py
@@ -0,0 +1,13 @@
+"""
+Introdução às funções (def) em Python
+Funções são trechos de código usados para
+replicar determinada ação ao longo do seu código.
+Elas podem receber valores para parâmetros (argumentos)
+e retornar um valor específico.
+Por padrão, funções em Pyuthon retornam None (nada).
+"""
+def soma(valor1, valor2):
+    return valor1 + valor2
+
+
+print(soma(2, 2))
